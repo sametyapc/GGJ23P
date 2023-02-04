6720b5baf3f712c24ab7e012c8cbd44d7f29eb98
Arda Dindar <ardadindar72@gmail.com>
2023-02-05 00:57:21 +0300

    CineMachine

--- Assets/Scripts/SpecialLevelScripts/Level1/CornerTurner.cs
+++ Assets/Scripts/SpecialLevelScripts/Level1/CornerTurner.cs
@@ -6,13 +6,17 @@ public class CornerTurner : MonoBehaviour
 {
     [SerializeField] float rotateValue;
     [SerializeField] bool goBattle;
+    [SerializeField] Animator cameraAnim;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            string stateName = goBattle ? "Battle" : "Follow";
+            cameraAnim.Play(stateName);
             other.transform.rotation = Quaternion.AngleAxis(rotateValue,transform.up);
             other.GetComponent<ShooterController>().InBattleStatus(goBattle);
+            
             gameObject.SetActive(false);
         }
     }
